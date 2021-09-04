edc5dba3989e5f67b326072fdd1d710470fc59a8
Samin <samin_ahmed@hotmail.com>
2021-09-04 22:17:10 +0100

    Created Listings page and created and merged to a new server

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
+# We created our models here following our Schema that we made. Also, we had to import from our "realtor" app 
+# the name "Realtor" as it's not defined in our Listings app (which is normal). 
+# Yet we have our "realtor" in our Listing class. So, we had to bring it in.
+# Plus, "blank=True" just means it's an optional field, the app won't complain if we leave it blank. 
+
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqft = models.IntegerField()
+    lot_size= models.DecimalField(max_digits=5, decimal_places=1)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.title
+
+
+    
+
--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+# Just like we did with listings we want a main field for the realtor, and we're gonna use their name.
+# Thus, we did the method and linked it to "name" in our class. 
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.name
+
+# Now we wanna put this all into the database. So, we need to make a migration and run it. 
+    
