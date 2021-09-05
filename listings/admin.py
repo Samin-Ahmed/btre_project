fb937887cce266f1b9107ea5a274744298b3e64a
Samin <samin_ahmed@hotmail.com>
2021-09-05 14:45:23 +0100

    Added changes to the listings and realtor pages on the admin section (local host) to increase usability (check admin.py to see the changes on both apps).

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from .models import Listing
 # Register your models here. We can customise our admin stuff for the listings app.
 
-admin.site.register(Listing)
+# The code below is to make edits to to listings page in the admin area - to add more data (price, list date, etc).
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    # This is to check and un-check the published button
+    list_editable = ('is_published',) 
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    # This is to add pagination (page 1, 2, etc to break up pages)
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Realtor
 # Register your models here.
+# We are changing and addings things to the realtor page on the admin page on local host. This will increase usability. 
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
