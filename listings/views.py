75cfdb7312b6e2ba6a1fcf2bf998da5e01ab4d8f
Samin <samin_ahmed@hotmail.com>
2021-09-05 21:41:25 +0100

    Amended the listings.html and views.py to begin the showing of dynamic listings

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Listing
 
 # Create your view methods here.
+# We have taken our database data and displayed it on our website 
+# (replacing the html static markup placeholders we had).
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
