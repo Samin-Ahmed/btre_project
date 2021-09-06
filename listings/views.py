02cb3a11f65963cd81d1b18af60773c2b9d2136b
Samin <samin_ahmed@hotmail.com>
2021-09-06 11:41:44 +0100

    Added a dynamic search form choices in the index

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 # Create your view methods here.
 # We have taken our database data and displayed it on our website 
@@ -30,4 +31,10 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context)
