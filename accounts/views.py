065a7e64a8850e6ab6decc1e39cdafeca190fc57
Samin <samin_ahmed@hotmail.com>
2021-09-07 10:18:42 +0100

    Added login method in accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render, redirect
 from django.contrib import messages 
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views methods here.
 
@@ -41,7 +42,18 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         # Login User
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+          auth.login(request, user)
+          messages.success(request, 'You are now logged in')
+          return redirect('dashboard')
+        else:
+          messages.error(request, 'Invalid credentials')
+          return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
