a1c3042aef52bd4382da3a2f821508e838d0de16
Samin <samin_ahmed@hotmail.com>
2021-09-08 10:52:11 +0100

    Added password encryption to settings.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from contacts.models import Contact
 from django.shortcuts import redirect, render, redirect
 from django.contrib import messages 
 from django.contrib.auth.models import User
@@ -64,4 +65,9 @@ def logout(request):
     return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+  context = {
+    'contacts': user_contacts
+  }
+  return render(request, 'accounts/dashboard.html', context)
--- btre/settings.py
+++ btre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': 'Kidsflame5@@',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': 'localhost'
     }
 }
@@ -155,5 +156,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'traversy.samin@legalutopia.com'
-EMAIL_HOST_PASSWORD = 'AeWTTvC3po54@@'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
